a189db749b2fc0f93c7615e94638c78c5dfdd328
faquan.yao <faquan.yao@gmail.com>
2021-07-25 15:36:28 +0800

    配置gradle打包应用的脚本

--- src/main/java/com/yaofaquan/SmartStock/Application.java
+++ src/main/java/com/yaofaquan/SmartStock/Application.java
@@ -1,4 +1,14 @@
 package com.yaofaquan.SmartStock;
 
+import java.io.IOException;
+
 public class Application {
+    public static void main(String[] argv) {
+        System.out.println("Hello Java Application!");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
